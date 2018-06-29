b2e56f3e0cc961bc8fa9084aed4b5f8b2e242050
zyken1 <35464034+zyken1@users.noreply.github.com>
2018-06-28 21:47:05 -0500

    add
    vistas separadas

--- app/src/main/java/com/example/nequiz_omen/recicler_view/MainActivity.java
+++ app/src/main/java/com/example/nequiz_omen/recicler_view/MainActivity.java
@@ -32,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
         names = this.getAllNames();  /*INSTANCIAMOS LA LISTA*/
         mRecyclerView = findViewById(R.id.recyclerView);             /*SE TRAE LA LISTA DESDE LA VISTA*/
         mLayoutManager = new LinearLayoutManager(this);      /*AQUI PUEDE SER LINEAR O  GRID O RELATIVE SE PUEDE CAMBIAR LA  VISTA*/
-        mLayoutManager = new GridLayoutManager(this,2);     //SE CAMBIA LA VISTA Y TE PIDE UN NUMERO DE COLUMNAS QUE SERAN 2
-        mLayoutManager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);  // Esta vista  te permite multiples layouts y los renderiza para una mejor vista
+        //mLayoutManager = new GridLayoutManager(this,2);     //SE CAMBIA LA VISTA Y TE PIDE UN NUMERO DE COLUMNAS QUE SERAN 2
+        //mLayoutManager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);  // Esta vista  te permite multiples layouts y los renderiza para una mejor vista
 
 
         mAdapter = new MyAdapter(names, R.layout.recycler_view_item, new MyAdapter.OnItemClickListener() {  /*names = es la lista , el layout = recycler_view_item y al no tener el listener se hace*/
