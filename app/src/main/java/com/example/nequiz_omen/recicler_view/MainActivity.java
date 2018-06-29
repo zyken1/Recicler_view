aa205c13a84872ad77cabd3a5367c2fa711d57d7
zyken1 <35464034+zyken1@users.noreply.github.com>
2018-06-28 21:43:46 -0500

    List y Grid
    061 Recycler View - List y Grid
    
    Principales layout manager qu epodemos tener

--- app/src/main/java/com/example/nequiz_omen/recicler_view/MainActivity.java
+++ app/src/main/java/com/example/nequiz_omen/recicler_view/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.nequiz_omen.recicler_view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -28,8 +30,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         names = this.getAllNames();  /*INSTANCIAMOS LA LISTA*/
-        mRecyclerView = findViewById(R.id.recyclerView);  /*SE TRAE LA LISTA DESDE LA VISTA*/
-        mLayoutManager = new LinearLayoutManager(this);  /*AQUI PUEDE SER LINEAR O  GRID O RELATIVE SE PUEDE CAMBIAR LA  VISTA*/
+        mRecyclerView = findViewById(R.id.recyclerView);             /*SE TRAE LA LISTA DESDE LA VISTA*/
+        mLayoutManager = new LinearLayoutManager(this);      /*AQUI PUEDE SER LINEAR O  GRID O RELATIVE SE PUEDE CAMBIAR LA  VISTA*/
+        mLayoutManager = new GridLayoutManager(this,2);     //SE CAMBIA LA VISTA Y TE PIDE UN NUMERO DE COLUMNAS QUE SERAN 2
+        mLayoutManager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);  // Esta vista  te permite multiples layouts y los renderiza para una mejor vista
+
+
         mAdapter = new MyAdapter(names, R.layout.recycler_view_item, new MyAdapter.OnItemClickListener() {  /*names = es la lista , el layout = recycler_view_item y al no tener el listener se hace*/
 
             @Override
